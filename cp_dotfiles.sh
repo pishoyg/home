e7a2f5865cd49b88bcdcc88216223aeb260971e8
Pishoy Girgis <pishoy.girgis@booking.com>
2023-10-11 11:36:14 +0200

    DEFAULT

--- cp_dotfiles.sh
+++ cp_dotfiles.sh
@@ -86,20 +86,20 @@ copy() {
 
 main() {
 
-  echo $'\n' ">>>>>>>>>> Copying files from ${HOME} to ${GIT_REPO}."
+  echo $'\n'">>>>>>>>>> Copying files from ${HOME} to ${GIT_REPO}."
   copy ~ "${GIT_REPO}"
 
-  echo $'\n' ">>>>>>>>>> Committing in ${GIT_REPO}."
+  echo $'\n'">>>>>>>>>> Committing in ${GIT_REPO}."
   git -C "${GIT_REPO}" add .
   git -C "${GIT_REPO}" commit --message "${MESSAGE}"
 
-  echo $'\n' ">>>>>>>>>> Pulling to ${GIT_REPO} from the remote repo."
+  echo $'\n'">>>>>>>>>> Pulling to ${GIT_REPO} from the remote repo."
   git -C "${GIT_REPO}" pull --rebase
 
-  echo $'\n' ">>>>>>>>>> Pushing from ${GIT_REPO} to the remote repo."
+  echo $'\n'">>>>>>>>>> Pushing from ${GIT_REPO} to the remote repo."
   git -C "${GIT_REPO}" push
 
-  echo $'\n' ">>>>>>>>>> Copying from ${GIT_REPO} to ${HOME}."
+  echo $'\n'">>>>>>>>>> Copying from ${GIT_REPO} to ${HOME}."
   copy "${GIT_REPO}" ~
 }
 
