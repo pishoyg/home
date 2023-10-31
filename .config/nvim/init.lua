cef220e4900f0d5b08884fec2205d67f3a0a6cfd
Pishoy Girgis <pishoybg@gmail.com>
2023-10-31 20:55:02 +0100

    Require Mason language servers in the Vim config.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -454,17 +454,17 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
-  -- pylsp = {
-  --   plugins = {
-  --     pycodestyle = { indentSize = 2 },
-  --     flake8 = { indentSize = 2 } },
-  --   indentSize = 2,
-  -- },
+  clangd = {},
+  gopls = {},
+  pyright = {},
+  rust_analyzer = {},
+  tsserver = {},
+  pylsp = {
+    plugins = {
+      pycodestyle = { indentSize = 2 },
+      flake8 = { indentSize = 2 } },
+    indentSize = 2,
+  },
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
